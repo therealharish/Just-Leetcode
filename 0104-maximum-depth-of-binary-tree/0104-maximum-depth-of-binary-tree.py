34caf9f64ec06d1d8ed91959ca56f474b30fe86d
Harish <48938639+therealharish@users.noreply.github.com>
2023-09-19 16:15:29 +0530

    Time: 53 ms (21.35%), Space: 18.5 MB (78.39%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -4,36 +4,12 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution1:
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
-        
-        def find(root, path):
-            nonlocal longestPath
-            if not root:
-                return 
-            
-            path.append(root)
-            
-            if(not root.left and not root.right):
-                longestPath = max(longestPath, len(path))
-            
-            find(root.left, path.copy())
-            find(root.right, path.copy())
-    
-        longestPath = 0            
-        find(root, [])
-        return longestPath
-                    
-
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
 
-        @cache
         def solve(root):
-
             if not root:
                 return 0
-            
-            return 1 + max(solve(root.left), solve(root.right))
-
+            return max(1 + solve(root.left), 1+solve(root.right))
         return solve(root)
+        
